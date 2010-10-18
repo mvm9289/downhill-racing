d0e01536017281e96a597eb7acb49b0bee4a3524
mvm9289 <mvm9289@06676c28-b993-0703-7cf1-698017e0e3c1>
2010-10-18 13:19:23 +0000

    Files tree modified

---
+++ Terrain.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Vertex.h"
+#include "Face.h"
+#include "Box.h"
+
+#include <gl/GL.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define TERRAIN_WIDTH 5.0
+#define TERRAIN_DEPTH 2.0
+#define VERTICES_PER_HEIGHT 2
+#define VERTICES_PER_FACE 4
+#define VERTEX_COMPONENTS 3
+#define NORMAL_COMPONENTS 3
+#define COLOR_COMPONENTS 3
+
+class Terrain
+{
+// ATTRIBUTES
+private:
+	vector<Vertex> vertices;
+	vector<Face> faces;
+	GLfloat* verticesArray;
+	GLfloat* normalsArray;
+	GLfloat* colorsArray;
+	GLint* indicesArray;
+
+public:
+	vector<int> heights;
+
+// FUNCTIONS
+private:
+	void readLevelTxt(string level);
+	void readLevelXML(string level);
+	void createTerrain(void);
+	void createVertexArrays(void);
+
+public:
+	Terrain(string level);
+	~Terrain(void);
+	Box boundingBox(void);
+	void render(void);
+};
