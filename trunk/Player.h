08f84c80feb654c42abf7832cc5badf28f8fd6ed
alarvi650 <alarvi650@06676c28-b993-0703-7cf1-698017e0e3c1>
2010-12-07 16:47:26 +0000

    - Afegides textures de player. - Afegit jump al player. - Avançar recte el player en cada iteració. - Modificació resize a MScreen.

--- trunk/Player.h
+++ trunk/Player.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Sphere.h"
 
+#define PLAYER_STEP 0.2
+#define JUMP_STEPS 15.0
+
 class Player : public Sphere {
 public:
 	Player(Point c = Point(0,0,0), float r = 1);
 	~Player(void);
 	void move(float dx);
 	void jump();
+	void advance();
 	Point getPosition();
 
 private:
 	float weight;
+	bool jumping;
+	float jumped;
 };
 
